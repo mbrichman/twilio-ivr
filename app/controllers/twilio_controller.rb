d5f039de55364cf31517358c4b0acaafa4189bf6
Mark Richman <mbrichman@gmail.com>
2014-02-11 10:51:54 -0600

    change it

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,20 @@
 class TwilioController < ApplicationController
 
   def hello
-
-  end
+    people = {
+      '+13122036261' => 'Curious George',
+      '+14158675310' => 'Boots',
+      '+14158675311' => 'Virgil',
+      '+14158675312' => 'Marcel',
+    }
+    puts params
+    name = people[params['From']] || 'Monkey'
+    Twilio::TwiML::Response.new do |r|
+      r.Say "Hello #{name}"
+      # r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
+      #   g.Say 'To speak to a real monkey, press 1.'
+      #   g.Say 'Press any other key to start over.'
+      end
+    end.text
 
 end
