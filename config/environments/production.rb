15e5c553465bd4e33cf1c0fc6895def6ebaffc13
Mark Richman <mbrichman@gmail.com>
2014-02-11 11:04:30 -0600

    logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ TwilioTest::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
