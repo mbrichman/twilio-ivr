f1a10bf89d364094e21108ce753626360dfa658c
Mark Richman <mbrichman@gmail.com>
2014-02-11 15:05:20 -0600

    add route for post

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ TwilioTest::Application.routes.draw do
   resources :appointments
 
   get '/hello' => 'twilio#hello'
+  post '/hello' => 'twilio#hello'
   get '/say_hi' => 'twilio#say_hi'
   post '/hello/options' => 'twilio#options'
 
