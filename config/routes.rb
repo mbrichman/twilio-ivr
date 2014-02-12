1004fb53628e65e4b244de0340ebb39598b92664
Mark Richman <mbrichman@gmail.com>
2014-02-12 16:05:02 -0600

    removed route as test

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ TwilioTest::Application.routes.draw do
   get '/say_hi/:id'     => 'twilio#say_hi'
   post '/hello/options' => 'twilio#options'
   post '/hello/times'   => 'twilio#parse_times'
-  get '/hello/times'    => 'twilio#parse_times'
+  # get '/hello/times'    => 'twilio#parse_times'
 
 end
