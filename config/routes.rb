f56ca902b996d0bcafe5a96d83d1c2a2e9c41922
Mark Richman <mbrichman@gmail.com>
2014-02-12 15:51:20 -0600

    get to post on routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ TwilioTest::Application.routes.draw do
   get '/hello'         => 'twilio#hello'
   post '/hello'        => 'twilio#hello'
   get '/say_hi/:id'    => 'twilio#say_hi'
-  get '/hello/options' => 'twilio#options'
+  post '/hello/options' => 'twilio#options'
   post '/hello/times'  => 'twilio#parse_times'
 
 end
